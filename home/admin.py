6bbf648c4662feea6d0fb5f379242f44821acad5
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-08-19 09:15:51 +0530

    34325

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,7 @@
-                                 <                                                <p>Price: {{ item.price }}</p>                              # views.py
-                                 from rest_framework.response import Response
-                                 from rest_framework.views import APIView
-                                 
-                                 class MenuView(APIView):
-                                     def get(self, request):
-                                             menu = [
-                                                         {"name": "Paneer Butter Masala", "description": "A popular Indian dish", "price": 15.99},
-                                                                     {"name": "Chicken Tikka Masala", "description": "A creamy and flavorful dish", "price": 14.99},
-                                                                                 {"name": "Samosas", "description": "Crispy fried or baked pastries", "price": 6.99},
-                                                                                             {"name": "Naan Bread", "description": "Leavened, butter-topped flatbread", "price": 3.99},
-                                                                                                     ]
-                                                                                                             return Response(menu)                                                                             r
+# admin.py
+from django.contrib import admin
+from .models import MenuItem, Order, OrderItem
+
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)                                                    r
