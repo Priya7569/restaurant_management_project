5d223a40de9e8ccace2eac17f8e6a1b8858f74df
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-08-19 07:49:37 +0530

    34322

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,12 @@
-<!-- footer.html -->
-<footer>
-    <p>&copy; {{ current_year }} {{ restaurant_name }}. All Rights Reserved.</p>
-        <p>Opening Hours:</p>
-            <p>Mon-Fri: 11am-9pm, Sat-Sun: 10am-10pm</p>
-            </footer>
-                                                                                      <label for=                                                                                                                                return render(request, 'menu.html', {'menu_items': menu_items})
+                            # models.py
+                            from django.db import models
+                            from django.contrib.auth.models import User
+                            
+                            class UserProfile(models.Model):
+                                user = models.OneToOneField(User, on_delete=models.CASCADE)
+                                    name = models.CharField(max_length=255)
+                                        email = models.EmailField(unique=True)
+                                            phone_number = models.CharField(max_length=20)
+                                            
+                                                def __str__(self):
+                                                        return self.user.username                                                                                                   return render(request, 'menu.html', {'menu_items': menu_items})
