8fa45298ed025465a2926423c12715ede1a579c1
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-01 15:58:37 +0530

    40809

--- home/admin.py
+++ home/admin.py
@@ -1,15 +1,10 @@
-from django.db import models
-
-class RestaurantLocation(models.Model):
-    address = models.CharField(max_length=255)
-        city = models.CharField(max_length=100)
-            state = models.CharField(max_length=50)
-                zip_code = models.CharField(max_length=10)
-                
-                    def __str__(self):
-                            return f"{self.address}, {self.city}, {self.state} {self.zip_code}"from django.conf import settings
 from django.shortcuts import render
+from .models import RestaurantLocation
 
 def home(request):
-    restaurant_name = settings.RESTAURANT_NAME
-        return render(request, 'home.html', {'restaurant_name': restaurant_name})
+    try:
+            restaurant_location = RestaurantLocation.objects.first()
+                except RestaurantLocation.DoesNotExist:
+                        restaurant_location = None
+
+                            return render(request, 'home.html', {'restaurant_location': restaurant_location})
