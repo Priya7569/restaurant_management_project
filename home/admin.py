2dce85be93bbaff631e0aa9b312c8429a3464416
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-01 15:55:36 +0530

    40813

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,13 @@
-from django.conf import settings
+from django.db import models
+
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=255)
+        city = models.CharField(max_length=100)
+            state = models.CharField(max_length=50)
+                zip_code = models.CharField(max_length=10)
+                
+                    def __str__(self):
+                            return f"{self.address}, {self.city}, {self.state} {self.zip_code}"from django.conf import settings
 from django.shortcuts import render
 
 def home(request):
