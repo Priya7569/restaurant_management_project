479016dc09af808b9c07b3c8b7233f8d2bd5e4c3
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-03 17:03:15 +0530

    41490

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,6 @@
-from django.db import models
-
-class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
-        address = models.CharField(max_length=255)
-            city = models.CharField(max_length=100)
-                state = models.CharField(max_length=50)
-                    zip_code = models.CharField(max_length=10)
-
-                        def __str__(self):
-                                return self.name
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-email-password'
