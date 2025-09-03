596e9bed363a0d0e3b0644910258970bc677d366
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-03 17:04:50 +0530

    41491

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,10 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'
+from django.db import models
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+        description = models.TextField()
+            price = models.DecimalField(max_digits=5, decimal_places=2)
+                image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+
+                    def __str__(self):
+                            return self.name
