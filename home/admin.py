864b53cabd8c69c40ec29fb19d99bee6e14e10e3
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-07 13:12:53 +0530

    42916

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,28 @@
-from django.db import models
+from django import forms
+from .models import ContactMessage
+from django.core.validators import EmailValidator
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
-        address = models.CharField(max_length=255)
-            city = models.CharField(max_length=100)
-                state = models.CharField(max_length=50)
-                    zip_code = models.CharField(max_length=10)
-                        opening_hours = models.JSONField(default=dict)
+class ContactForm(forms.ModelForm):
+    class Meta:
+            model = ContactMessage
+                    fields = ('name', 'email', 'message')
+                            widgets = {
+                                        'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
+                                                    'email': forms.EmailInput(attrs={'placeholder': 'Your Email'}),
+                                                                'message': forms.Textarea(attrs={'placeholder': 'Your Message'}),
+                                                                        }
 
-                            def __str__(self):
-                                    return self.name
+                                                                            def clean_email(self):
+                                                                                    email = self.cleaned_data.get('email')
+                                                                                            validator = EmailValidator()
+                                                                                                    try:
+                                                                                                                validator(email)
+                                                                                                                        except forms.ValidationError:
+                                                                                                                                    raise forms.ValidationError("Invalid email address.")
+                                                                                                                                            return email
+
+                                                                                                                                                def clean_message(self):
+                                                                                                                                                        message = self.cleaned_data.get('message')
+                                                                                                                                                                if len(message) < 10:
+                                                                                                                                                                            raise forms.ValidationError("Message should be at least 10 characters long.")
+                                                                                                                                                                                    return message
