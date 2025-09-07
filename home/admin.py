0807671a50d1fa070f6da88bd1e7a40f758bdc90
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-07 13:20:41 +0530

    42917

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,8 @@
 # views.py
 from django.shortcuts import render
+from .models import Cart
 
-def about(request):
-    return render(request, 'about.html')
+def homepage(request):
+    cart_items = Cart.objects.filter(user=request.user)
+        total_items = sum(item.quantity for item in cart_items)
+            return render(request, 'homepage.html', {'total_items': total_items})
