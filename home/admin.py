f8fdcbc8ae4fe84379ad99a5448b655dac313365
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-07 13:19:05 +0530

    42918

--- home/admin.py
+++ home/admin.py
@@ -1,28 +1,5 @@
-from django import forms
-from .models import ContactMessage
-from django.core.validators import EmailValidator
+# views.py
+from django.shortcuts import render
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-            model = ContactMessage
-                    fields = ('name', 'email', 'message')
-                            widgets = {
-                                        'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
-                                                    'email': forms.EmailInput(attrs={'placeholder': 'Your Email'}),
-                                                                'message': forms.Textarea(attrs={'placeholder': 'Your Message'}),
-                                                                        }
-
-                                                                            def clean_email(self):
-                                                                                    email = self.cleaned_data.get('email')
-                                                                                            validator = EmailValidator()
-                                                                                                    try:
-                                                                                                                validator(email)
-                                                                                                                        except forms.ValidationError:
-                                                                                                                                    raise forms.ValidationError("Invalid email address.")
-                                                                                                                                            return email
-
-                                                                                                                                                def clean_message(self):
-                                                                                                                                                        message = self.cleaned_data.get('message')
-                                                                                                                                                                if len(message) < 10:
-                                                                                                                                                                            raise forms.ValidationError("Message should be at least 10 characters long.")
-                                                                                                                                                                                    return message
+def about(request):
+    return render(request, 'about.html')
