308fe07110474e5377ced4d58848e6e4a86653e2
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-07 13:25:30 +0530

    42920

--- home/admin.py
+++ home/admin.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
-        logo = models.ImageField(upload_to='restaurant_logos', blank=True, null=True)
+        phone_number = models.CharField(max_length=20, blank=True, null=True)
             # Add other fields as needed
