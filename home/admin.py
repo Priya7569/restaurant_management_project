2dc7bc39b1ffcb551c1d7a35f3baf56ec2a2e67c
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-10 14:22:51 +0530

    43267

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,11 @@
-# templatetags/breadcrumbs.py
-from django import template
-register = template.Library()
+<!-- home.html -->
+{% extends 'base.html' %}
 
-@register.inclusion_tag('breadcrumbs.html')
-def breadcrumbs(request):
-    path = request.path.strip('/').split('/')
-        crumbs = []
-            url = ''
-                for item in path:
-                        url += f'/{item}'
-                                crumbs.append({'label': item.capitalize(), 'url': url})
-                                    return {'crumbs': crumbs}
+{% block content %}
+    <div class="welcome-section">
+            <h1>Welcome to Flavors of India</h1>
+                    <p class="tagline">Experience the authentic tastes of India in a warm and inviting atmosphere</p>
+                        </div>
+                            <!-- other content -->
+                            {% endblock %}
+                            
