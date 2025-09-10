cb5e1962a252a668f239155a2254159fd98e3dea
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-10 14:21:41 +0530

    43266

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,13 @@
-# views.py
-from django.shortcuts import render
+# templatetags/breadcrumbs.py
+from django import template
+register = template.Library()
 
-def faq(request):
-    faqs = [
-            {"question": "What are your operating hours?", "answer": "We are open Monday to Saturday, 11 AM to 11 PM."},
-                    {"question": "Do you offer delivery?", "answer": "Yes, we offer delivery through various partners in the city."},
-                            {"question": "Can I book a table in advance?", "answer": "Yes, you can book a table by calling us or using our online booking system."},
-                                    {"question": "Do you have vegetarian options?", "answer": "Yes, we have a variety of vegetarian dishes on our menu."},
-                                        ]
+@register.inclusion_tag('breadcrumbs.html')
+def breadcrumbs(request):
+    path = request.path.strip('/').split('/')
+        crumbs = []
+            url = ''
+                for item in path:
+                        url += f'/{item}'
+                                crumbs.append({'label': item.capitalize(), 'url': url})
+                                    return {'crumbs': crumbs}
