ba178abb2edf26476732d8c19bc1c00f4fe30d30
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-17 14:07:05 +0530

    43849

--- home/admin.py
+++ home/admin.py
@@ -1,16 +1,17 @@
-<form method="post" action="{% url 'login' %}">
-  {% csrf_token %}
-    <div>
-        <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-              </div>
-                <div>
-                    <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" required>
-                          </div>
-                            {% if form.errors %}
-                                <p style="color: red;">Invalid username or password.</p>
-                                  {% endif %}
-                                    <button type="submit">Login</button>
-                                    </form>
-                                                  <label for=
+from django import template
+from django.urls import resolve
+
+register = template.Library()
+
+@register.inclusion_tag('breadcrumbs.html')
+def breadcrumbs(request):
+    path = request.path
+        # Simple path parsing for breadcrumbs
+            parts = path.strip('/').split('/')
+                crumbs = []
+                    url = ''
+                        for part in parts:
+                                if part:
+                                            url += '/' + part
+                                                        crumbs.append({'label': part.capitalize(), 'url': url})
+                                                            return {'breadcrumbs': crumbs}
