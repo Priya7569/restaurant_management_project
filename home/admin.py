34696d11073b6b7a86447f7ce12deb6a296d73ea
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-17 14:03:26 +0530

    43847

--- home/admin.py
+++ home/admin.py
@@ -1,1 +1,16 @@
-<a href="order-placement.html" class="order-now-btn">Order Now</a>
+<form method="post" action="{% url 'login' %}">
+  {% csrf_token %}
+    <div>
+        <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+              </div>
+                <div>
+                    <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" required>
+                          </div>
+                            {% if form.errors %}
+                                <p style="color: red;">Invalid username or password.</p>
+                                  {% endif %}
+                                    <button type="submit">Login</button>
+                                    </form>
+                                    
