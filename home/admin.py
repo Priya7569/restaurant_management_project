59700c9b985a8fb066a5acaeecfd63146778333a
Priya7569 <haripriyajonnalagadda0@gmail.com>
2025-09-17 14:08:47 +0530

    43850

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,5 @@
-from django import template
-from django.urls import resolve
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
-register = template.Library()
-
-@register.inclusion_tag('breadcrumbs.html')
-def breadcrumbs(request):
-    path = request.path
-        # Simple path parsing for breadcrumbs
-            parts = path.strip('/').split('/')
-                crumbs = []
-                    url = ''
-                        for part in parts:
-                                if part:
-                                            url += '/' + part
-                                                        crumbs.append({'label': part.capitalize(), 'url': url})
-                                                            return {'breadcrumbs': crumbs}
+class PrivacyPolicyView(TemplateView):
+    template_name = 'privacy_policy.html'
